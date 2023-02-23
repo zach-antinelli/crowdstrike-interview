3ce967150230f75e1f791c177ca4fa630cde14e6
Zach Antinelli <zachantinelli@gmail.com>
2023-02-22 23:36:30 -0800

    init

--- script-examples/openai_terminal.py
+++ script-examples/openai_terminal.py
@@ -3,8 +3,8 @@
 import sys
 import openai
 
-'''
-Interact with the OpenAI API using the davinci engine.
+"""
+Interact with the OpenAI API using the davinci model.
 
 You must set the environment variable "OPENAI_API_KEY"
     
@@ -12,27 +12,27 @@ Usage:
             
     Non-interactive:
         
-        python chatgpt-terminal.py "Should I HODL?"
+        python openai_terminal.py "Should I HODL?"
 
     Interactive:
 
-        python chatgpt-terminal.py interactive
+        python openai_terminal.py interactive
 
-        python chatgpt-terminal.py -i
+        python openai_terminal.py -i
 
-            chatgpt>> Should I HODL?
+            openai>> Should I HODL?
 
             It depends on your individual goals and risk tolerance. HODLing is a strategy that involves holding a cryptocurrency 
             for a long period of time, usually in the hopes of it increasing in value. If you are comfortable with the risks associated 
             with cryptocurrency investing and believe that the potential rewards are worth it, then HODLing may be a good strategy for 
             you. However, it is important to remember that cryptocurrencies are volatile and the market can be unpredictable, so it is 
             important to do your own research and make sure you understand the risks before investing.
-'''
+"""
 
 def non_interactive(prompt):
-    '''
+    """
     Non-interactive prompt, run the script with a string argument.
-    '''
+    """
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -44,13 +44,13 @@ def non_interactive(prompt):
     return completions.choices[0].text.strip()
 
 def interactive():
-    '''
+    """
     Interactive prompt, ask as many questions as you like.
-    '''
+    """
     try:
         while True:
-            prompt = input('chatgpt>> ')
-            if prompt in ('exit', 'Exit', 'quit', 'Quit', 'bye', 'Bye'):
+            prompt = input("openai>> ")
+            if prompt in ("exit", "Exit", "quit", "Quit", "bye", "Bye"):
                 sys.exit()
             completions = openai.Completion.create(
                 engine="text-davinci-003",
@@ -60,34 +60,35 @@ def interactive():
                 stop=None,
                 temperature=0.5,
             )
-            print('\n' + completions.choices[0].text.strip() + '\n')
+            print("\n" + completions.choices[0].text.strip() + "\n")
     except KeyboardInterrupt:
-        sys.exit('\nKeyboard interrupt received, exiting..')
+        sys.exit("\nKeyboard interrupt received, exiting..")
 
 def main():
-    if len(sys.argv) > 2 :
-        print('''
-
-            Incorrect parameter or amount of parameters entered.
-
-            Usage:
+    if len(sys.argv) != 2 or sys.argv[1] in ("-h","--help"):
+        print("""
+        Usage:
+            
+            Non-interactive example:
                 
-                Non-interactive example:
-                    
-                    python chatgpt-terminal.py "Should I HODL?"
+                python openai_terminal.py "Should I HODL?"
 
-                Interactive example:
+                ./openai_terminal.py "What is love?"
 
-                    python chatgpt-terminal.py interactive
+            Interactive example:
 
-                    python chatgpt-terminal.py -i
+                python openai_terminal.py -i
 
-            ''')
+                python openai_terminal.py interactive
+
+                ./openai_terminal --interactive
+
+            """)
         sys.exit(1)
     input_arg = sys.argv[1]
-    if input_arg == 'interactive' or input_arg == '-i':
+    if input_arg in ("interactive","-i","--interactive"):
         interactive()
     print(non_interactive(input_arg))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
